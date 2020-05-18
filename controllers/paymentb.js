998bdbfd923a45e06fbd74b6ce472c14e349ab1b
Ashish jagrawal <akjagrawal24@gmail.com>
2020-05-18 12:31:19 +0530

    first commit

---
+++ controllers/paymentb.js
@@ -0,0 +1,41 @@
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "445xhzyxmxmmkxmv",
+  publicKey: "wm9bg4zpkvzv28bg",
+  privateKey: "0849fa7d123b4ebf6af796333dc5d08e"
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, function(err, response) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+
+  let amountFromTheClient = req.body.amount;
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+
+      options: {
+        submitForSettlement: true
+      }
+    },
+    function(err, result) {
+      if (err) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
+};
